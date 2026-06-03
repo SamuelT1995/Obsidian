4defb51562d05fdeba70929e39a199f35de487b5
samuelT1995 <samueltenagne1995@gmail.com>
2026-06-04 01:59:00 +0300

    chore: export Products and Process from sections

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,3 +1,5 @@
 export { default as Hero } from "./Hero";
 export { default as Manifesto } from "./Manifesto";
 export { default as Origin } from "./Origin";
+export { default as Products } from "./Products";
+export { default as Process } from "./Process";
