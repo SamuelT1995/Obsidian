48805fa2fdf05cd19886fc38b71fd73cdba9f37b
samuelT1995 <samueltenagne1995@gmail.com>
2026-06-04 01:49:28 +0300

    chore: export Navbar from components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as CustomCursor } from "./CustomCursor";
 export { default as ScrollProgress } from "./ScrollProgress";
 export { default as LenisProvider } from "./LenisProvider";
 export { default as PageTransition } from "./PageTransition";
+export { default as Navbar } from "./Navbar";
